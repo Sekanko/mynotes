24ffbd978be095ad9611493d6f05d02a963b6b58
Oskar Tarka <oskar0208@gmail.com>
2025-03-19 00:10:14 +0100

    step-10

--- lib/services/auth/auth_service.dart
+++ lib/services/auth/auth_service.dart
@@ -19,7 +19,7 @@ class AuthService implements AuthProvider {
   AuthUser? get currentUser => provider.currentUser;
 
   @override
-  Future<AuthUser?> logIn({
+  Future<AuthUser> logIn({
     required String email,
     required String password,
   }) =>

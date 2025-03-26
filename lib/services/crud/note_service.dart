5173de585e8ad080d38b9f25ed8462d3e063de12
Oskar Tarka <oskar0208@gmail.com>
2025-03-26 22:51:02 +0100

    step-17

--- lib/services/crud/note_service.dart
+++ lib/services/crud/note_service.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:path/path.dart' show join;
 import 'package:mynotes/services/crud/crud_exceptions.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
 
 class NoteService {
   // fields

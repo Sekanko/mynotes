51abf3d59bd177269a3422a6d01f9e3dc295892c
Oskar Tarka <oskar0208@gmail.com>
2025-03-27 20:14:19 +0100

    step 19

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,16 +3,19 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class AuthUser {
+  final String id;
   final bool isEmailVerified;
-  final String? email;
+  final String email;
 
   const AuthUser({
     required this.isEmailVerified,
     required this.email,
+    required this.id,
   });
 
   factory AuthUser.fromFirebase(User user) => AuthUser(
+        id: user.uid,
         isEmailVerified: user.emailVerified,
-        email: user.email,
+        email: user.email!,
       );
 }
--- test/auth_test.dart
+++ test/auth_test.dart
@@ -117,6 +117,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: 'test',
       isEmailVerified: false,
       email: 'test@mail.com',
     );
@@ -138,6 +139,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
+      id: 'test',
       isEmailVerified: true,
       email: 'test@mail.com',
     );
